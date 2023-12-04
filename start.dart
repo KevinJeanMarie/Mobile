9b8d16bbf387ebee73fcb92959abdb37a4b763cb
Jean Marie Kévin <kjeanmarie01@gmail.com>
2023-12-04 21:05:10 +0100

    in progress..

--- start.dart
+++ start.dart
@@ -5,6 +5,7 @@ void main() {
   print("Hello World");
 
   startGame();
+  guesscolor();
 }
 
 //1
@@ -73,7 +74,7 @@ void startGame() {
                 "Vous tombez sur un groupe de survivants qui vous accueillent à bras ouvert dans leur abri");
             break;
         }
-        break;
+        return;
       case 4:
         print("Merci d'avoir joué. Au revoir !");
         break;
@@ -85,3 +86,35 @@ void startGame() {
 }
 
 //2
+void guesscolor() {
+  List<String> colors = ["Rouge", "Vert", "Bleu", "Jaune", "Violet"];
+
+  // couleur choisie aléatoirement
+  Random random = Random();
+  String hiddenColor = colors[random.nextInt(colors.length)];
+
+  while (true) {
+    //afficher les options
+    print("Devinez la couleur cachée :");
+    for (int i = 0; i < colors.length; i++) {
+      print("${i + 1}. ${colors[i]}");
+    }
+
+    // demande au user faire un choix
+    print("Votre choix (1-5) : ");
+    int userChoice = int.parse(stdin.readLineSync()!);
+
+    // verification si le choix du user correspond à la couleur
+    if (userChoice >= 1 && userChoice <= colors.length) {
+      String userColor = colors[userChoice - 1];
+
+      if (userColor == hiddenColor) {
+        print(
+            "Féliciations ! Vous avez deviné la couleur correcte : $hiddenColor");
+        break;
+      } else {
+        print("Dommage ! La couleur choisie n'est pas la bonne. Réessayez.");
+      }
+    }
+  }
+}
